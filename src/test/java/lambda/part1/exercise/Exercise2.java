f02acc96475054e920e93ad1013f4fb576369c8e
maksvyacheslavovich <maksvyacheslavovich@gmail.com>
2018-09-25 02:27:27 +0300

    part_1 exercise_2 completed

--- src/test/java/lambda/part1/exercise/Exercise2.java
+++ src/test/java/lambda/part1/exercise/Exercise2.java
@@ -24,21 +24,26 @@ class Exercise2 {
     @Test
     void implementsIntegerMultiplierUsingAnonymousClass() {
         Multiplier<Integer> multiplier = null;
-
+        multiplier = new Multiplier<Integer>() {
+            @Override
+            public Integer multiply(Integer value, int multiplier) {
+                return value * multiplier;
+            }
+        };
         testIntegerMultiplier(multiplier);
     }
 
     @Test
     void implementsMultiplierUsingStatementLambda() {
         Multiplier<Integer> multiplier = null;
-
+        multiplier = (v1, v2) ->{ return v1 * v2;};
         testIntegerMultiplier(multiplier);
     }
 
     @Test
     void implementsIntegerMultiplierUsingExpressionLambda() {
         Multiplier<Integer> multiplier = null;
-
+        multiplier = (v1, v2) -> v1 * v2;
         testIntegerMultiplier(multiplier);
     }
 
@@ -58,10 +63,18 @@ class Exercise2 {
         return builder.toString();
     }
 
+    private static String multiply(String value, int multiplier) {
+            if(multiplier == 0) return "";
+            StringBuilder sb = new StringBuilder(value);
+            for (int i= 1; i < multiplier; i++ )
+                sb.append(value);
+            return sb.toString();
+    }
+
     @Test
     void implementsStringMultiplierUsingClassMethodReference() {
         Multiplier<String> multiplier = null;
-
+        multiplier = Exercise2::multiply;
         assertThat(multiplier.multiply("a", 3), is("aaa"));
         assertThat(multiplier.multiply("qwerty", 0), is(emptyString()));
         assertThat(multiplier.twice("aA"), is("aAaA"));
@@ -82,7 +95,8 @@ class Exercise2 {
     @Test
     void implementsStringMultiplierUsingObjectMethodReference() {
         Multiplier<String> multiplier = null;
-
+        Exercise2 ex = new Exercise2();
+        multiplier = ex::stringSumWithDelimiter;
         assertThat(multiplier.multiply("a", 3), is("a-a-a"));
         assertThat(multiplier.multiply("qwerty", 0), is(emptyString()));
         assertThat(multiplier.twice("A"), is("A-A"));
