935777cbb0a6e77e9fcc89a1ae88e28449cd74fe
maksim_malkov <maksim_malkov@epam.com>
2018-10-01 19:20:22 +0300

    delete unneeded comment

--- src/test/java/lambda/part3/exercise1/Exercise1.java
+++ src/test/java/lambda/part3/exercise1/Exercise1.java
@@ -29,7 +29,6 @@ public class Exercise1 {
         Function<String, Integer> stringLengthExtractor = String::length;
         // TODO функция извлечения длины полного имени из сотрудника fullNameLengthExtractor: Employee -> Integer
         Function<Employee, Integer> fullNameLengthExtractor =
-//                employee -> stringLengthExtractor.apply(fullNameExtractor.apply(personExtractor.apply(employee)));
                 employee -> personExtractor.andThen(fullNameExtractor).andThen(stringLengthExtractor).apply(employee);
         // TODO преобразование списка employees в lengths используя fullNameLengthExtractor
         employees.forEach(employee -> lengths.add(fullNameLengthExtractor.apply(employee)));
